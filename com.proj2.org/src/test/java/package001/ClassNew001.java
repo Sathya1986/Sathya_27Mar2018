ca38efe98a20c4bebbcf5141ec261d34cf52f1b1
s.a.shanmugam <s.a.shanmugam@BDC6-LX-21GL5M2.dir.svc.accenture.com>
2018-03-28 08:17:07 +0530

    First Commit

---
+++ com.proj2.org/src/test/java/package001/ClassNew001.java
@@ -0,0 +1,22 @@
+package package001;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ClassNew001 {
+	
+	WebDriver driver;
+	
+	@Test
+	public void test2()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\s.a.shanmugam\\\\Desktop\\\\Sathya_Personal\\\\Selenium\\\\chromedriver_win32\\\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		driver.get("http://www.google.com");
+		
+		
+	}
+
+}
